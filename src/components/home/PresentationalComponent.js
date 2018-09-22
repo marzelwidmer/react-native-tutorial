046ea5f63ebb899727dd3f4133792bd284b76939
Marcel Widmer <marzelwidmer@gmail.com>
2018-09-22 19:21:13 +0200

    Styling with StyleSheet

--- src/components/home/PresentationalComponent.js
+++ src/components/home/PresentationalComponent.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, StyleSheet} from 'react-native'
 
 const PresentationalComponent = (props) => {
     return (
         <View>
-            <Text onPress={props.updateState}>
+            <Text style={styles.myState} onPress={props.updateState}>
                 {props.myState}
             </Text>
         </View>
     )
 }
 export default PresentationalComponent
+
+const styles = StyleSheet.create({
+    myState: {
+        backgroundColor: "#0D0D0D",
+        marginTop: 20,
+        textAlign: 'center',
+        color: 'white',
+        fontWeight: '200',
+        fontSize: 20
+    }
+})
+
