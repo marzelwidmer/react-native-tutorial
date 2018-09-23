a20be0d9638e60f9d002a5817050eea34849ccd8
Marcel Widmer <marzelwidmer@gmail.com>
2018-09-23 08:29:18 +0200

    ListView with Map and Alert

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,73 +1,80 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import List from './List.js'
 
-const Home = (props) =>  {
+const Home = () => {
     return (
-        <View>
-            <View style = {style.container1}>
-                <View style = {style.blackImg}/>
-                <View style = {style.yellowImg}/>
-            </View>
-            <View style = {style.container2}>
-                <View style = {style.blackImg}/>
-                <View style = {style.yellowImg}/>
-            </View>
-
-            <View style = {style.container3}>
-                <View style = {style.blackImg}/>
-                <View style = {style.yellowImg}/>
-            </View>
-
-            <View style = {style.container4}>
-                <View style = {style.blackImg}/>
-                <View style = {style.yellowImg}/>
-            </View>
-
-            <View style = {style.container5}>
-                <View style = {style.blackImg}/>
-                <View style = {style.yellowImg}/>
-            </View>
-        </View>
+        <List />
     )
 }
+
+
+// const Home = (props) =>  {
+//     return (
+//         <View>
+//             <View style = {style.container1}>
+//                 <View style = {style.blackImg}/>
+//                 <View style = {style.yellowImg}/>
+//             </View>
+//             <View style = {style.container2}>
+//                 <View style = {style.blackImg}/>
+//                 <View style = {style.yellowImg}/>
+//             </View>
+//
+//             <View style = {style.container3}>
+//                 <View style = {style.blackImg}/>
+//                 <View style = {style.yellowImg}/>
+//             </View>
+//
+//             <View style = {style.container4}>
+//                 <View style = {style.blackImg}/>
+//                 <View style = {style.yellowImg}/>
+//             </View>
+//
+//             <View style = {style.container5}>
+//                 <View style = {style.blackImg}/>
+//                 <View style = {style.yellowImg}/>
+//             </View>
+//         </View>
+//     )
+// }
 export default Home
 
-const style = StyleSheet.create ({
-    container1: {
-        borderBottomWidth: 1,
-        borderBottomColor: '#f4c842'
-    },
-    container2: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderBottomColor: '#f4c842'
-    },
-    container3: {
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        borderBottomWidth: 1,
-        borderBottomColor: '#f4c842'
-    },
-    container4: {
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderBottomColor: '#f4c842'
-    },
-    container5: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    blackImg: {
-        backgroundColor: 'black',
-        height: 90,
-        width: 90
-    },
-    yellowImg: {
-        backgroundColor: 'yellow',
-        height: 50,
-        width: 50
-    }
-})
+// const style = StyleSheet.create ({
+//     container1: {
+//         borderBottomWidth: 1,
+//         borderBottomColor: '#f4c842'
+//     },
+//     container2: {
+//         flexDirection: 'row',
+//         justifyContent: 'space-around',
+//         alignItems: 'center',
+//         borderBottomWidth: 1,
+//         borderBottomColor: '#f4c842'
+//     },
+//     container3: {
+//         flexDirection: 'row',
+//         justifyContent: 'flex-end',
+//         borderBottomWidth: 1,
+//         borderBottomColor: '#f4c842'
+//     },
+//     container4: {
+//         alignItems: 'center',
+//         borderBottomWidth: 1,
+//         borderBottomColor: '#f4c842'
+//     },
+//     container5: {
+//         flexDirection: 'row',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//     },
+//     blackImg: {
+//         backgroundColor: 'black',
+//         height: 90,
+//         width: 90
+//     },
+//     yellowImg: {
+//         backgroundColor: 'yellow',
+//         height: 50,
+//         width: 50
+//     }
+// })
