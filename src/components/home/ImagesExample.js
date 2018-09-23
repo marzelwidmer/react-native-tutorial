2d3ab1a3e8117e78a22e56c2d3e1eb1ac4598917
Marcel Widmer <marzelwidmer@gmail.com>
2018-09-23 09:07:09 +0200

    Images

--- src/components/home/ImagesExample.js
+++ src/components/home/ImagesExample.js
@@ -2,7 +2,13 @@ import React, {Component} from 'react'
 import {Image} from 'react-native'
 
 const Home = () => (
-    <Image source={require('../../img/icon.png')}
+    <Image
+        // Local Image
+            source={require('../../img/icon.png')}
+        // Network Image
+        // source = {{ uri:
+        //         'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png' }}
+
            style={{width: 200, height: 200, top: 30, left: 5}}/>
 
 )
