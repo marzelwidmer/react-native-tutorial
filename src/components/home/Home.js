4d63e9bfc176661bef913f6bffa60244cd866dde
Marcel Widmer <marzelwidmer@gmail.com>
2018-09-24 09:29:14 +0200

    design route-flux NavBar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
 import { Actions } from "react-native-router-flux";
 
 const Home = () => {
@@ -10,19 +10,49 @@ const Home = () => {
     Actions.webViewExample();
   };
   return (
-    <View>
+    <View style={styles.container2}>
       <View>
-        <Text>Welcome ...</Text>
+        <Text style={styles.welcome}>Welcome ...</Text>
       </View>
       <View>
-        <TouchableOpacity style={{margin: 10 }} onPress={goToAbout}>
-          <Text>This is HOME!</Text>
+        <TouchableOpacity style={{ margin: 10 }} onPress={goToAbout}>
+          <Text style={styles.home}>This is HOME!</Text>
         </TouchableOpacity>
         <TouchableOpacity style={{ margin: 10 }} onPress={gotToWebViewExample}>
-          <Text>WebViewExample</Text>
+          <Text style={styles.webView}>WebViewExample</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 };
 export default Home;
+
+const styles = StyleSheet.create({
+  container2: {
+    flex: 1,
+    backgroundColor: "#0D0D0D",
+    alignItems: "center",
+    paddingTop: 130,
+    paddingHorizontal: 20
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#191919"
+  },
+  welcome: {
+    fontSize: 60,
+    textAlign: "center",
+    color: "#FAFAFA",
+    marginBottom: 5,
+    margin: 10
+  },
+  home: {
+    color: "#424242"
+  }, 
+  webView: {
+    color: "#EEEEEE"
+  }
+});
+ 
--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import { Router, Scene } from 'react-native-router-flux'
-import Home from '../home/Home'
-import WebViewExample from '../webView/WebViewExample'
-import About from '../about/About'
+import React from "react";
+import { Router, Scene, Navigator } from "react-native-router-flux";
+import Home from "../home/Home";
+import WebViewExample from "../webView/WebViewExample";
+import About from "../about/About";
+import { StyleSheet } from "react-native";
 
 const Routes = () => (
-   <Router>
-      <Scene key = "root">
-         <Scene key = "home" component = {Home} title = "Home" initial = {true} />
-         <Scene key = "webViewExample" component = {WebViewExample} title = "WebViewExample" />
-         <Scene key = "about" component = {About} title = "About" />
-      </Scene>
-   </Router>
-)
-export default Routes
+  <Router navigationBarStyle={styles.navBar} titleStyle={styles.navTitle}>
+    <Scene key="root">
+      <Scene
+        key="home"
+        component={Home}
+        title="Home"
+        initial={true}
+        leftButtonIconStyle={styles.backButton}
+      />
+      <Scene
+        key="webViewExample"
+        component={WebViewExample}
+        title="WebViewExample"
+        leftButtonIconStyle={styles.backButton}
+      />
+      <Scene
+        key="about"
+        backTitle="Zrog"
+        backButtonTextStyle={styles.navTitle}
+
+        navBarButtonColor="red"
+        backButtonTintColor="red"
+        component={About}
+        title="About"
+      />
+    </Scene>
+  </Router>
+);
+export default Routes;
+
+const styles = StyleSheet.create({
+  navBar: {
+    backgroundColor: "#0D0D0D" // changing navbar color
+  },
+  navTitle: {
+    color: "#EEEEEE" // changing navbar title color
+  }
+});
